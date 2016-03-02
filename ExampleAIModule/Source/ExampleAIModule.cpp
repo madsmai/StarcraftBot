73a15e288d38a7d46076c0d95974d7a735cd5325
DantesGearbox <mads.engberg@hotmail.com>
2016-03-02 17:35:19 +0100

    Revert "Test"
    This reverts commit ee5ae6071ba6559ba52ba6bf76e8ee60e866dc55.

--- ExampleAIModule/Source/ExampleAIModule.cpp
+++ ExampleAIModule/Source/ExampleAIModule.cpp
@@ -13,7 +13,7 @@ bool analysis_just_finished;
 void ExampleAIModule::onStart()
 {
 	// Hello World!
-	Broodwar->sendText("Hello ME!");
+	Broodwar->sendText("Hello World!");
 
 	// Print the map name.
 	// BWAPI returns std::string when retrieving a string, don't forget to add .c_str() when printing!
