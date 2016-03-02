4140fdbea74f3f22979def437843ce02b3f20549
DantesGearbox <mads.engberg@hotmail.com>
2016-03-02 18:38:13 +0100

    Made it work

--- ExampleAIModule/Source/ExampleAIModule.h
+++ ExampleAIModule/Source/ExampleAIModule.h
@@ -1,62 +1,8 @@
 #pragma once
 #include <BWAPI.h>
-#include <vector> // for std::vector
-#include <BWTA.h>
-#include <windows.h>
-
-// Remember not to use "Broodwar" in any global class constructor!
-
-DWORD WINAPI AnalyzeThread();
 
 class ExampleAIModule : public BWAPI::AIModule
 {
 public:
-
-	bool refinery;
-	bool refineryFinished;
-	int refineryWorkers;
-	int workers;
-	int mineralsReserved;
-
-	bool firstPylon;
-	BWAPI::Unit scout;
-
-	std::set<TilePosition> startPositions;
-
-	std::set<BWTA::BaseLocation*>& baseLocations;
-
-
-
-	void releaseMinerals(BWAPI::Unit unit);
-	void constructBuilding(BWAPI::UnitType buildingType, BWAPI::Unit worker);
-	void supplyCheckAndBuild(BWAPI::Unit worker);
-	void goScout(BWAPI::Unit scout);
-
-	void drawTerrainData();
-
-	std::vector<int> pendingBuildings;
-
-
-	// Virtual functions for callbacks, leave these as they are.
 	virtual void onStart();
-	virtual void onEnd(bool isWinner);
-	virtual void onFrame();
-	virtual void onSendText(std::string text);
-	virtual void onReceiveText(BWAPI::Player player, std::string text);
-	virtual void onPlayerLeft(BWAPI::Player player);
-	virtual void onNukeDetect(BWAPI::Position target);
-	virtual void onUnitDiscover(BWAPI::Unit unit);
-	virtual void onUnitEvade(BWAPI::Unit unit);
-	virtual void onUnitShow(BWAPI::Unit unit);
-	virtual void onUnitHide(BWAPI::Unit unit);
-	virtual void onUnitCreate(BWAPI::Unit unit);
-	virtual void onUnitDestroy(BWAPI::Unit unit);
-	virtual void onUnitMorph(BWAPI::Unit unit);
-	virtual void onUnitRenegade(BWAPI::Unit unit);
-	virtual void onSaveGame(std::string gameName);
-	virtual void onUnitComplete(BWAPI::Unit unit);
-	// Everything below this line is safe to modify.
-
-
-
 };
