5972bdf1f965624093613355943e812101c09260
DantesGearbox <mads.engberg@hotmail.com>
2016-03-02 18:34:24 +0100

    Testing

--- ExampleAIModule/Source/ExampleAIModule.cpp
+++ ExampleAIModule/Source/ExampleAIModule.cpp
@@ -13,7 +13,7 @@ bool analysis_just_finished;
 void ExampleAIModule::onStart()
 {
 	// Hello World!
-	Broodwar->sendText("Hi World!");
+	Broodwar->sendText("Hi ME!");
 
 	// Print the map name.
 	// BWAPI returns std::string when retrieving a string, don't forget to add .c_str() when printing!
