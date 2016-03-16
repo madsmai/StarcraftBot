5dcce44cf4e0071f41888d649154f2341949edca
saladoor <sghali@hotmail.com>
2016-03-16 15:03:52 +0100

    Updated sscout manager

--- ExampleAIModule/ScoutManager.h
+++ ExampleAIModule/ScoutManager.h
@@ -1,16 +1,31 @@
 #pragma once
 
 #include <BWAPI.h>
+#include <vector> // for std::vector
+#include <BWTA.h>
 
 class ScoutManager
 {
 public:
+
+	BWAPI::Unit scout;
+
+
+
 	ScoutManager();
 	~ScoutManager();
 
 	void onFrame();
 	void onUnitDestroy(BWAPI::Unit unit);
 	void onUnitDiscover(BWAPI::Unit unit);
+	void goScout(BWAPI::Unit scout);
+
+	//BWTA variables
+	BWTA::BaseLocation* scoutedBase;
+	BWTA::BaseLocation* enemyBase;
+	BWTA::BaseLocation* expansion;
+	BWTA::BaseLocation* ourBase;
+	std::set<BWTA::BaseLocation*> baseLocations;
 
 	ScoutManager& ScoutManager::getInstance();
 };
