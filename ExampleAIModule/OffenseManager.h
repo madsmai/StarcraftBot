7b76c94d42c6839684031edebc8545b712406316
Mads <mads.e.maibohm@gmail.com>
2016-03-30 14:45:04 +0200

    Implemented undertower feature

--- ExampleAIModule/OffenseManager.h
+++ ExampleAIModule/OffenseManager.h
@@ -41,6 +41,7 @@ private:
 	bool fightBack(BWAPI::Unit);
 	bool getHelp(BWAPI::Unit, BWAPI::Unit);
 	bool searchAndDestroy(BWAPI::Unitset);
+	bool avoidTowers(BWAPI::Unit);
 
 	OffenseManager() {};
 	~OffenseManager() {};
