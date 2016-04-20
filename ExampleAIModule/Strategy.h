b1c5aa9045727b11b34077ebf1ff39174fcc9a14
Mads <mads.e.maibohm@gmail.com>
2016-04-20 10:26:00 +0200

    Quality of life for strategy coding

--- ExampleAIModule/Strategy.h
+++ ExampleAIModule/Strategy.h
@@ -7,12 +7,29 @@ using namespace BWAPI;
 class Strategy {
 public:
 
+
 	enum strategies { justSomeStrategy, standardZealotRush };
 
 	Strategy(int strategy);
 	std::string getName(){ return name; }
 	bool getStarter(){ return starter; }
 	std::vector<BuildOrderType> const & getStrategyQueue(){ return strategyQueue; }
+
+	//BuildOrderTypes for easy access
+	BuildOrderType probe = BuildOrderType(UnitTypes::Protoss_Probe);
+	BuildOrderType pylon = BuildOrderType(UnitTypes::Protoss_Pylon);
+	BuildOrderType gateway = BuildOrderType(UnitTypes::Protoss_Gateway);
+	BuildOrderType scoutRequest = BuildOrderType(BuildOrderType::requests::scoutRequest);
+	BuildOrderType assimilator = BuildOrderType(UnitTypes::Protoss_Assimilator);
+	BuildOrderType gasworkerRequest = BuildOrderType(BuildOrderType::requests::gasworkerRequest);
+	BuildOrderType zealot = BuildOrderType(UnitTypes::Protoss_Zealot);
+	BuildOrderType cybercore = BuildOrderType(UnitTypes::Protoss_Cybernetics_Core);
+	BuildOrderType dragoon = BuildOrderType(UnitTypes::Protoss_Dragoon);
+	BuildOrderType citadel = BuildOrderType(UnitTypes::Protoss_Citadel_of_Adun);
+	BuildOrderType dtemplar = BuildOrderType(UnitTypes::Protoss_Dark_Templar);
+	BuildOrderType archives = BuildOrderType(UnitTypes::Protoss_Templar_Archives);
+	BuildOrderType forge = BuildOrderType(UnitTypes::Protoss_Forge);
+	BuildOrderType cannon = BuildOrderType(UnitTypes::Protoss_Photon_Cannon);
 
 private:
 	std::string name;
