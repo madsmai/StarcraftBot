ebfd478d4763d12e371d2f5dbdfe6eeffe7817c9
madsmai <mads.e.maibohm@gmail.com>
2016-05-04 09:29:03 +0200

    Date and time on log

--- ExampleAIModule/OffenseManager.cpp
+++ ExampleAIModule/OffenseManager.cpp
@@ -6,24 +6,35 @@ using namespace BWAPI;
 /*
 TODO:
 - Expansions bugger os, helt vildt.
-	- De bevæger sig frem og tilbage
-	- Dræber ikke workers i expansion først
-	- Tror det er fordi de bliver sat til at bevæge sig mod hovedbasen, når den, bliver idle, laver search and destroy.
+- De bevæger sig frem og tilbage
+- Dræber ikke workers i expansion først
+- Tror det er fordi de bliver sat til at bevæge sig mod hovedbasen, når den, bliver idle, laver search and destroy.
 - Abuse andre bots dårlig micro
 - Vi gør ikke noget mod et rush der er hurtigere end vores
-	-Ikke nogle metoder til at få vores zealots til at gøre noget i basen
+-Ikke nogle metoder til at få vores zealots til at gøre noget i basen
 
 - Early dark templars strategien bugger
 
 - Fjern casen for 1 enemyAttacker i fightback
 
 
+- Crash forårsaget af
+
+if (fighters.find(attackedUnit) != fighters.end()) {
+fighters.erase(fighters.find(attackedUnit));
+}
+
+
 */
 
 void OffenseManager::onUnitDestroy(BWAPI::Unit unit){
 	if (unit->getPlayer() == Broodwar->self()) {
+		InformationManager::getInstance().writeToLog("One of our dudes died");
 		if (fighters.find(unit) != fighters.end()) {
 			fighters.erase(fighters.find(unit));
+		}
+		else if (cowards.find(unit) != cowards.end()) {
+			cowards.erase(cowards.find(unit));
 		}
 	}
 }
@@ -40,7 +51,9 @@ void OffenseManager::onUnitComplete(BWAPI::Unit unit){
 
 void OffenseManager::onFrame(){
 	for (Unit unit : fighters) {
-		if (unit->isUnderAttack()) {
+		InformationManager::getInstance().writeToLog("Entered for loop");
+		if (unit != NULL && unit->isUnderAttack()) {
+			InformationManager::getInstance().writeToLog("Under attack");
 			if (InformationManager::getInstance().calculateArmyStrength(Broodwar->self()) < InformationManager::getInstance().calculateArmyStrength(Broodwar->enemy())
 				&& BWTA::getNearestBaseLocation(unit->getPosition()) != InformationManager::getInstance().ourBase) {
 
@@ -53,23 +66,32 @@ void OffenseManager::onFrame(){
 			}
 		}
 		else if (InformationManager::getInstance().enemyBase != NULL
+			&& InformationManager::getInstance().enemyBase->getRegion() != NULL
+			&& BWTA::getRegion(unit->getTilePosition()) != NULL
 			&& InformationManager::getInstance().enemyBase->getRegion() == BWTA::getRegion(unit->getTilePosition())
 			&& unit->isIdle()) {
+			InformationManager::getInstance().writeToLog("If checks worked out1");
 			searchAndDestroy(unit);
 		}
-		else if (InformationManager::getInstance().ourBase->getRegion() == BWTA::getRegion(unit->getTilePosition())
+		else if (InformationManager::getInstance().ourBase != NULL
+			&& InformationManager::getInstance().ourBase->getRegion() != NULL
+			&& BWTA::getRegion(unit->getTilePosition()) != NULL
+			&& InformationManager::getInstance().ourBase->getRegion() == BWTA::getRegion(unit->getTilePosition())
 			&& unit->isIdle()
 			&& !rushOngoing){
+			InformationManager::getInstance().writeToLog("If checks worked out2");
 
-			unit->move(BWTA::getNearestBaseLocation(InformationManager::getInstance().ourBase->getPosition())->getPosition());
+			unit->move(InformationManager::getInstance().ourBase->getPosition());
 			squad.insert(unit);
 		}
+		InformationManager::getInstance().writeToLog("Got through it, phew");
 	}
 	if (squad.size() >= armySize) {
 		rush(squad);
 		squad.clear();
 	}
 	if (cowards.size() > 0 && runFrames + 18 <= Broodwar->getFrameCount()) {
+		InformationManager::getInstance().writeToLog("Started Coward purging");
 		for (Unit coward : cowards) {
 			if (fighters.find(coward) == fighters.end()) {
 				fighters.insert(coward);
@@ -118,15 +140,16 @@ bool OffenseManager::fightBack(BWAPI::Unit attackedUnit) {
 		BWAPI::Unitset nearbyAllies = attackedUnit->getUnitsInRadius(128, !Filter::IsEnemy && !Filter::IsWorker && !Filter::IsBuilding);
 		if (attacker != NULL) {
 			if (attackedUnit->getShields() < attackedUnit->getType().maxShields() / 10
-				&& nearbyAllies.size()>0
-				&& nearbyEnemies.size()>1) {
+				&& nearbyAllies.size() > 0
+				&& nearbyEnemies.size() > 1) {
 
 				if (cowards.find(attackedUnit) == cowards.end()) {
 					cowards.insert(attackedUnit);
 				}
-				if (fighters.find(attackedUnit) != fighters.end()) {
+				if (fighters.size() > 0 && fighters.find(attackedUnit) != fighters.end()) {
 					fighters.erase(fighters.find(attackedUnit));
 				}
+
 				runFrames = Broodwar->getFrameCount();
 
 				if (BWTA::getNearestBaseLocation(attackedUnit->getPosition()) == InformationManager::getInstance().enemyBase) {
@@ -135,6 +158,7 @@ bool OffenseManager::fightBack(BWAPI::Unit attackedUnit) {
 				else {
 					attackedUnit->move(BWTA::getNearestChokepoint(attackedUnit->getPosition())->getCenter());
 				}
+				InformationManager::getInstance().writeToLog("Got past it");
 			}
 			else if (nearbyEnemies.size() >= 2) {
 				int maxPrio = 0;
