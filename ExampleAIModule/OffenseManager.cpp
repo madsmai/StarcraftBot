9eadf14f388407796afac967630e02156a50ea7d
madsmai <mads.e.maibohm@gmail.com>
2016-06-12 14:55:57 +0200

    Fixed crash on last building

--- ExampleAIModule/OffenseManager.cpp
+++ ExampleAIModule/OffenseManager.cpp
@@ -6,9 +6,12 @@ TODO:
 
 - Abuse andre bots dårlig micro
 
-- Avoid towers virker ikke ordentligt, den mener alt er under tower
 
 - Troops skal ikke bevæge sig ud af basen med mindre der ikke er nogle enemies i basen.
+
+- Hvis vores scout dør kan vi ikke angribe
+
+-
 
 */
 
@@ -205,36 +208,46 @@ void OffenseManager::searchAndDestroy(BWAPI::Unit attacker) {
 		closest = attacker->getClosestUnit((Filter::CanAttack || Filter::IsSpellcaster) && Filter::CanMove && Filter::IsEnemy, 1240);
 		if (properClosestTarget(closest, attacker)) {
 			attacker->attack(closest);
+
+			FixWrongPriority(closest);
 		}
 	}
 	else if (!InformationManager::getInstance().enemyTowers.empty()) {
 		closest = attacker->getClosestUnit(Filter::IsBuilding && (Filter::CanAttack || Filter::GetType == UnitTypes::Terran_Bunker) && Filter::IsEnemy, 1240);
 		if (properClosestTarget(closest, attacker)) {
 			attacker->attack(closest);
+
+			FixWrongPriority(closest);
 		}
 	}
 	else if (!InformationManager::getInstance().enemyWorkers.empty()) {
 		closest = attacker->getClosestUnit(Filter::IsWorker && Filter::IsEnemy, 1240);
 		if (properClosestTarget(closest, attacker)) {
 			attacker->attack(closest);
+
+			FixWrongPriority(closest);
 		}
 	}
 	else if (!InformationManager::getInstance().enemyBarracks.empty()) {
 		closest = attacker->getClosestUnit(Filter::CanProduce && Filter::IsBuilding && !Filter::IsResourceDepot && Filter::IsEnemy, 1240);
 		if (properClosestTarget(closest, attacker)) {
 			attacker->attack(closest);
+
+			FixWrongPriority(closest);
 		}
 	}
 	else if (!InformationManager::getInstance().enemyPassiveBuildings.empty()) {
 		closest = attacker->getClosestUnit(Filter::IsBuilding && !Filter::CanAttack && Filter::IsEnemy && Filter::IsVisible, 1240);
 		if (properClosestTarget(closest, attacker)) {
 			attacker->attack(closest);
+
+			FixWrongPriority(closest);
 		}
 	}
 	//Finding other units that should also attack this
-	if (properClosestTarget(closest, attacker)) {
-	FixWrongPriority(closest);
-	}
+	/*if (properClosestTarget(closest, attacker)) {
+		FixWrongPriority(closest);
+	}*/
 	if (attacker->isIdle() 
 		&& InformationManager::getInstance().enemyBase != NULL
 		&& BWTA::getRegion(attacker->getTilePosition()) != InformationManager::getInstance().ourBase->getRegion()) {
@@ -258,17 +271,24 @@ int OffenseManager::calculatePriority(Unit enemy, Unit ourUnit) {
 			int ourDamage = (Broodwar->self()->damage(ourUnit->getType().groundWeapon()) - enemy->getPlayer()->armor(enemy->getType())) * ourUnit->getType().maxGroundHits();
 
 			//Integer division round up
-			int hitsToKill = (effectiveHp + (ourDamage - 1)) / ourDamage;
+			int hitsToKill;
+			if (ourDamage != 0) {
+				hitsToKill = (effectiveHp + (ourDamage - 1)) / ourDamage;
+			}
+			else {
+				hitsToKill = effectiveHp + (ourDamage - 1);
+			}
+			
 
 			/*int damage = (enemy->getPlayer()->damage(enemy->getType().groundWeapon()) - Broodwar->self()->armor(ourUnit->getType())) * enemy->getType().maxGroundHits();*/
 
-			int damage = (enemy->getType().groundWeapon().damageAmount - Broodwar->self()->armor(ourUnit->getType())) * enemy->getType().maxGroundHits();
+			int damage = (enemy->getType().groundWeapon().damageAmount() - Broodwar->self()->armor(ourUnit->getType())) * enemy->getType().maxGroundHits();
 
 			int priority = damage / hitsToKill;
 
 			return priority + 100;
 		}
-		else if (enemy->getType().isBuilding() && enemy->getType().canAttack()) {
+		else if (enemy->getType().isBuilding() && (enemy->getType().canAttack() || enemy->getType() == UnitTypes::Terran_Bunker)) {
 			//Its a tower
 			return 90;
 		}
@@ -338,7 +358,7 @@ void OffenseManager::fillReaverOrCarrier(Unit unit){
 }
 
 bool OffenseManager::properClosestTarget(BWAPI::Unit target, BWAPI::Unit attacker) {
-	return target != NULL 
+	return target != NULL && attacker != NULL
 		&& target->isVisible() 
 		&& BWTA::isConnected(attacker->getTilePosition(), target->getTilePosition())
 		&& target->exists() 
