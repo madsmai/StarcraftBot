1443b1eb6b7497acf06e27dcccbd0573b8b5a786
Mads <mads.e.maibohm@gmail.com>
2016-06-17 15:37:10 +0200

    Carrier fix 3.0

--- ExampleAIModule/InformationManager.cpp
+++ ExampleAIModule/InformationManager.cpp
@@ -336,7 +336,7 @@ int InformationManager::calculateArmyStrength(BWAPI::Unitset fighters) {
 		if (u->isVisible()) {
 			if (u->getType() == UnitTypes::Protoss_Carrier) {
 				int effectiveHp = u->getType().maxHitPoints() + u->getType().maxShields();
-				int damage = u->getInterceptorCount * Broodwar->self()->damage(UnitTypes::Protoss_Interceptor.groundWeapon());
+				int damage = u->getInterceptorCount() * Broodwar->self()->damage(UnitTypes::Protoss_Interceptor.groundWeapon());
 				sum += effectiveHp * damage;
 				continue;
 			}
